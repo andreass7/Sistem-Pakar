c748e9a1f2418673c3116d8f95c3c0b8c4ec8503
Ghufron03 <62162384+Ghufron03@users.noreply.github.com>
2024-11-07 12:10:15 +0700

    about
    update about

--- resources/views/components/user/navbar.blade.php
+++ resources/views/components/user/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">Your Company</span>
-                <img class="h-10 w-auto" src="{{ asset('img/log.png') }}" alt="">
+                <img class="h-10 w-auto" src="{{ asset('img/logo-light.png') }}" alt="">
             </a>
         </div>
         <div class="flex lg:hidden">
